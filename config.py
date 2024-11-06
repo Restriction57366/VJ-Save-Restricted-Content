8610a02689a3131b82a97d7723dbc057b69e0c7f
Restriction57366 <tryout57366@gmail.com>
2024-11-06 15:12:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "22401377"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "6858d7e073194911666cd103613b1b15
+")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://tryout57366: JmcWIrw85S0GGqAE@cluster0.y6zub.mongodb.net/")
